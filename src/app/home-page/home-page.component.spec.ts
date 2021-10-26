ea94aadaca543f69fa0ff7bd7c8e3fe152299316
harrywhiteman <13564024@student.uts.edu.au>
2021-10-26 15:18:05 +1100

    fixed a couple of testing errors

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomePageComponent } from './home-page.component';
 
@@ -8,7 +10,8 @@ describe('HomePageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
+      declarations: [ HomePageComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
     })
     .compileComponents();
   });
